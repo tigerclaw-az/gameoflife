f3939686ca686d61fb1f55e23a36a1ae9dac6262
Erik Waibel <ejwaibel@gmail.com>
2014-06-04 15:06:33 -0700

    Update connect.js for grunt

--- grunt/connect.js
+++ grunt/connect.js
@@ -1,37 +1,16 @@
 module.exports = {
 	options: {
 		port: 9000,
-		// change this to '0.0.0.0' to access the server from outside
 		hostname: 'localhost'
 	},
 	livereload: {
 		options: {
-			middleware: function (connect) {
-				return [
-					lrSnippet,
-					mountFolder(connect, '.tmp'),
-					mountFolder(connect, yeomanConfig.app)
-				];
-			}
-		}
-	},
-	test: {
-		options: {
-			middleware: function (connect) {
-				return [
-					mountFolder(connect, '.tmp'),
-					mountFolder(connect, 'test')
-				];
-			}
+			base: '<%= appPath %>'
 		}
 	},
 	dist: {
 		options: {
-			middleware: function (connect) {
-				return [
-					mountFolder(connect, yeomanConfig.dist)
-				];
-			}
+			base: '<%= distPath %>'
 		}
 	}
 };
