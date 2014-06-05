f9288ba72a161a737dea8cb902191d6f556e275e
Erik Waibel <ejwaibel@gmail.com>
2014-06-05 10:29:34 -0700

    Change jQuery load functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,3 @@
-jQuery(document).ready(function($) {
-	var board = new Board(45, 45, $('#board'));
-
-	$('#new-board').click(function() {
-		board.clear();
-		board = newBoard(board);
-	});
-
-	$('#next').click(function() {
-		board.next();
-	});
-});
-
 function newBoard(board) {
 	for (var i = 0; i < 345; ++i) {
 		board.birth(
@@ -21,3 +8,16 @@ function newBoard(board) {
 
 	return board;
 }
+
+(function($) {
+	var board = new Board(45, 45, $('#board'));
+
+	$('#new-board').click(function() {
+		board.clear();
+		board = newBoard(board);
+	});
+
+	$('#next').click(function() {
+		board.next();
+	});
+})(jQuery);
