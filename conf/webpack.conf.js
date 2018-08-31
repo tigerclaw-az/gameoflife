cdfb0b9ad8462bbb16838769dfbe02ecc2eb1ae3
Erik Waibel <erik@irelo.com>
2018-08-30 20:25:24 -0700

    Fixes bug with bootstrap glyphicons not being loaded properly

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -42,6 +42,16 @@ module.exports = {
         loaders: [
           'html-loader'
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       }
     ]
   },
